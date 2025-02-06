1714f54b1ca75c596fb4456c12a0e223bdb930b9
subscriptionvineeth <subscriptionvineeth@gmail.com>
2025-02-06 20:03:39 +0530

    Fix: image added 7

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -5,11 +5,16 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 const baseUrl = import.meta.env.VITE_APP_BASE_URL || '';
 const techCompanies = [
-  { name: 'Microsoft', logo: `/assets/logo1.svg` },
-  { name: 'Google', logo: `./assets/logo2.svg` },
-  { name: 'Apple', logo: `/assets/logo3.svg` },
-  { name: 'Amazon', logo: `/assets/logo5.svg` },
-  
+  { name: 'Microsoft', logo: `${baseUrl}/assets/logo1.svg` },
+  { name: 'Google', logo: `${baseUrl}/assets/logo2.svg` },
+  { name: 'Apple', logo: `${baseUrl}/assets/logo3.svg` },
+  { name: 'Amazon', logo: `${baseUrl}/assets/logo4.svg` },
+  { name: 'Meta', logo: `${baseUrl}/assets/logo5.svg` },
+  { name: 'IBM', logo: `${baseUrl}/assets/logo6.svg` },
+  { name: 'Intel', logo: `${baseUrl}/assets/logo7.svg` },
+  { name: 'Oracle', logo: `${baseUrl}/assets/logo8.svg` },
+  { name: 'Salesforce', logo: `${baseUrl}/assets/logo9.svg` },
+  { name: 'Adobe', logo: `${baseUrl}/assets/logo10.svg` }
 ];
 
 const Clients = () => {
